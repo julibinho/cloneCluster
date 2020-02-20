1f60c0b19637e61392a91c472cb84d7d1715a9e0
Lisa1234 <lisa.chabrier@outlook.fr>
2020-02-20 15:09:43 +0100

    modification du path.sh

--- path.sh
+++ path.sh
@@ -5,10 +5,23 @@
 #read -p 'Quel fichier souhaitez vous utiliser ? ' fichier
 #DATA="data/artficial/Extracted_CDR3/$fichier.fa"
 
-read -p 'Entez le type de donnée souhaitée parmi mono, oligo ou poly : ' type
+read -p 'Entrez le type de donnée souhaitée parmi mono, oligo ou poly : ' type
+read -p 'Entrez la longueur de séquences souhaitées parmi CDR3 ou Ent : ' long
 
-export DATA="$PWD/data/artficial/Extracted_CDR3/$type""clonal_simp_indel_cdr3.fa"
-export DATAENT="$PWD/data/artficial/entireSeq/$type""clonal_simp_indel.fasta"
+
+if [ "$long" = "CDR3" ]
+then
+    export DATA="$PWD/data/artficial/Extracted_CDR3/$type""clonal_simp_indel_cdr3.fa"
+fi
+
+if [ "$long" = "Ent" ]
+then
+    export DATA="$PWD/data/artficial/entireSeq/$type""clonal_simp_indel.fasta"
+fi
+
+
+#export DATA="$PWD/data/artficial/Extracted_CDR3/$type""clonal_simp_indel_cdr3.fa"
+#export DATAENT="$PWD/data/artficial/entireSeq/$type""clonal_simp_indel.fasta"
 #echo $DATA
 #echo DATA=data/artficial/Extracted_CDR3/oligoclonal_simp_indel_cdr3.fa 
 #echo $DATA
