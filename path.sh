6c4d9b39cb7e2d2d8dfbf4a1f718464d1182c554
Lisa1234 <lisa.chabrier@outlook.fr>
2020-04-04 19:50:36 +0200

    correction de path.sh pour les Real data

--- path.sh
+++ path.sh
@@ -23,20 +23,64 @@ fi
 
 
 
-
-if [ "$long" = "Ent" ]
+if [ "$caract" = "Artificial"]
 then
-	export DATA="$PWD/data/Artificial/EntireSeq/$Type""clonal_simp_indel_cdr3.fa"
+    if [ "$long" = "Ent" ]
+    then
+	    export DATA="$PWD/data/$caract/EntireSeq/$Type""clonal_simp_indel.fa"
+	    export RESULT="$PWD/result/$caract/$algo/$long/$Type""clonal_simp_indel.txt"
+    fi
+
+    if [ "$long" = "CDR3" ]
+    then
+	    export DATA="$PWD/data/$Caract/Extracted_CDR3/$Type""clonal_simp_indel_cdr3.fa"
+	    export RESULT="$PWD/result/$caract/$algo/CDR3/$Type""clonal_simp_indel_cdr3.txt"
+    fi
 fi
 
-if [ "$long" = "CDR3" ]
+if [ "$caract" = "Real"]
 then
-	export DATA="$PWD/data/Artificial/Extracted_CDR3/$Type""clonal_simp_indel_cdr3.fa"
-fi
+    if [ "$long" = "Ent" ]
+    then
+        if [ "$Type" = "mono" ]
+        then
+            export DATA="$PWD/data/$caract/EntireSeq/I3_mono.fa"
+	        export RESULT="$PWD/result/$caract/$algo/$long/$Type""clonal_simp_indel.txt"
+	    fi
+	    if [ "$Type" = "oligo" ]
+        then
+            export DATA="$PWD/data/$caract/EntireSeq/I1_oligo.fa"
+	        export RESULT="$PWD/result/$caract/$algo/$long/$Type""clonal_simp_indel.txt"
+	    fi
+	    if [ "$Type" = "poly" ]
+        then
+            export DATA="$PWD/data/$caract/EntireSeq/I4_poly.fa"
+	        export RESULT="$PWD/result/$caract/$algo/$long/$Type""clonal_simp_indel.txt"
+	    fi
+    fi
 
+    if [ "$long" = "CDR3" ]
+    then
+        if [ "$Type" = "mono" ]
+        then
+            export DATA="$PWD/data/$Caract/Extracted_CDR3/I3_CDR3_mono.fa"
+	        export RESULT="$PWD/result/$caract/$algo/CDR3/$Type""clonal_simp_indel_cdr3.txt"
+	    fi
+	    if [ "$Type" = "oligo" ]
+        then
+            export DATA="$PWD/data/$Caract/Extracted_CDR3/I1_CDR3_oligo.fa"
+	        export RESULT="$PWD/result/$caract/$algo/CDR3/$Type""clonal_simp_indel_cdr3.txt"
+	    fi
+	    if [ "$Type" = "poly" ]
+        then
+            export DATA="$PWD/data/$Caract/Extracted_CDR3/I4_CDR3_poly.fa"
+	        export RESULT="$PWD/result/$caract/$algo/CDR3/$Type""clonal_simp_indel_cdr3.txt"
+	    fi
+    fi
+fi
 
 #Les algos : 
-export RESULT="$PWD/result/$caract/$algo/$long/$Type""clonal_simp_indel_cdr3.txt"
+
 export F1="$PWD/evaluation/F1-score/cluster_based_fscore.py"
 export SILHOUETTE="$PWD/evaluation/Silhouette/Silhouette.py"
 export TIME="$PWD/evaluation/Exec_time/Exec_time.py"
@@ -45,4 +89,4 @@ export GRAPH="$PWD/evaluation/Visualisation/show-graph.py"
 export ANALYSIS="$PWD/evaluation/Analysis-Recall/essai2.py"
 export TESTBENCH="$PWD/algoCluster/Test_Bench/test_bench.py"
 export REF="$PWD/data/Artificial/True_clusters/$Type""clonal_simp_indel_true_clusters.txt"
-export RESIMGT="$PWD/data/Tools_output/IMGT_output/Simulated_data/$Type""_simp_indel_imgt_Fo.txt"
+export RES_IMGT="$PWD/data/Tools_output/IMGT_output/Simulated_data/$Type""_simp_indel_imgt_Fo.txt"
