a2e2bfbb28ae812f32468acd905d3238a431ee77
Lisa1234 <lisa.chabrier@outlook.fr>
2020-04-09 18:24:14 +0200

    correction de path.sh, ajout d'un script pour le merging et premiers essais (pas très concluants)

--- algoCluster/Merging/Merging.py
+++ algoCluster/Merging/Merging.py
@@ -7,8 +7,8 @@ pwd = os.getcwd()
 sys.path.append(pwd+'/tool') # Fonctionne sur windows et linux, et permet d'indiquer dans quel fichier sont les modules. 
 import argparse
 import community ###### ALGO DE LOUVAIN #######
-
-import graph_input 
+import consensus
+#import graph_input 
 import result_output
 
 ###############################################################
@@ -52,13 +52,15 @@ def exec(path_to_file): # utilisé par Exec_Time pour timer l'algo
 ############################################################
 
 def main():
-	args = parse_arguments()
-	path_to_file = args.path_to_file
-	path_to_result = args.result
+	#args = parse_arguments()
+	#path_to_file = args.path_to_file
+	#path_to_result = args.result
+	path_to_file = "/home/lisa/Programmation/cloneCluster/data/Tools_output/IMGT_output/Simulated_data/mono_simp_indel_imgt_Fo.txt"
+	path_to_data = "/home/lisa/Programmation/cloneCluster/data/Artificial/Extracted_CDR3/monoclonal_simp_indel_cdr3.fa"
+	path_to_result = "/home/lisa/Programmation/cloneCluster/algoCluster/Merging/test_artificial_mono.txt"
 	
-	
-	dico_des_graphes, dico_init = graph_input.generate_graphs_and_init(path_to_file)
-	
+	dico_des_graphes = consensus.generate_graphs_consensus(path_to_file,path_to_data)
+	print(dico_des_graphes)
 	partitions = {}
 	
 	#dico_des_mat = graph_input.generate_matrix(path_to_file) 
@@ -67,7 +69,8 @@ def main():
 		#print('la matrice des distances pèse : ', sys.getsizeof(dico_des_mat[w]))
 		#print('le graphe de taille ', w, 'contenant ', len(dico_init[w]), 'séquences prends : ', size_nx(dico_des_graphes[w]))
 		#print(dico_init[w],'\n')
-		partitions[w] = community.best_partition(dico_des_graphes[w], dico_init[w])
+		partitions[w] = community.best_partition(dico_des_graphes[w])
+	
 	result_output.generate_output_text(partitions,path_to_result) #permet de générer le fichier texte qui ensuite sert à comparer les résultats aux true clusters
 
 
